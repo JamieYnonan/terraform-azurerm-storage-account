fa16d362af821e3cfb34a209523de71dedf9bfaa
Jamie Ynonan <jamiea31@gmail.com>
2025-06-07 00:54:06 -0500

    fix(locals): enable shared access key

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   min_tls_version                    = "TLS1_2"
   allow_nested_items_to_be_public    = false
   local_user_enabled                 = false
-  shared_access_key_enabled          = false
+  shared_access_key_enabled          = true
   blob_properties_versioning_enabled = true
   blob_propertieschange_feed_enabled = true
   container_access_type              = "private"
